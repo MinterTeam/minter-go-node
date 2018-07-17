5e517a92673c82c3b5645bad8c846cb63dd7d6c0
Daniil Lashin <danil-lashin@yandex.ru>
2018-07-17 13:24:27 +0300

    divide commissions by 10

--- core/commissions/commissions.go
+++ core/commissions/commissions.go
@@ -1,7 +1,7 @@
 package commissions
 
-// all commissions are divided by 10^15
-// actual commission is SendTx * 10^15 = 1 000 000 000 000 000 PIP = 0,001 BIP
+// all commissions are divided by 10^14
+// actual commission is SendTx * 10^14 = 10 000 000 000 000 000 PIP = 0,01 BIP
 const (
 	SendTx                int64 = 10
 	ConvertTx             int64 = 1000
@@ -9,7 +9,6 @@ const (
 	DeclareCandidacyTx    int64 = 10000
 	DelegateTx            int64 = 100
 	UnbondTx              int64 = 100
-	RedeemCheckTx         int64 = 10
 	PayloadByte           int64 = 2
 	ToggleCandidateStatus int64 = 100
 )
