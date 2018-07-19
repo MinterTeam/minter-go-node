d3e0e0fb39beb6221cd1a21b378024fb711c781c
Daniil Lashin <danil-lashin@yandex.ru>
2018-07-19 15:54:43 +0300

    refactor config

--- config/config.go
+++ config/config.go
@@ -13,20 +13,21 @@ func GetConfig() *tmConfig.Config {
 	config.P2P.PersistentPeers = "249c62818bf4601605a65b5adc35278236bd5312@95.216.148.138:26656"
 
 	config.Moniker = "MinterNode"
+
 	config.TxIndex = &tmConfig.TxIndexConfig{
 		Indexer:      "kv",
 		IndexTags:    "",
 		IndexAllTags: true,
 	}
+
 	config.DBPath = MinterDir + "/tmdata"
+
 	config.Mempool.CacheSize = 100000
 	config.Mempool.WalPath = MinterDir + "/tmdata/mempool.wal"
-	config.Consensus.WalPath = MinterDir + "/tmdata/cs.wal/wal"
-	config.PrivValidator = MinterDir + "/config/priv_validator.json"
-	config.NodeKey = MinterDir + "/config/node_key.json"
-	config.P2P.AddrBook = MinterDir + "/config/addrbook.json"
-	config.P2P.ListenAddress = "tcp://0.0.0.0:26656"
+	config.Mempool.Recheck = true
+	config.Mempool.RecheckEmpty = true
 
+	config.Consensus.WalPath = MinterDir + "/tmdata/cs.wal/wal"
 	config.Consensus.TimeoutPropose = 3000
 	config.Consensus.TimeoutProposeDelta = 500
 	config.Consensus.TimeoutPrevote = 1000
@@ -35,5 +36,11 @@ func GetConfig() *tmConfig.Config {
 	config.Consensus.TimeoutPrecommitDelta = 500
 	config.Consensus.TimeoutCommit = 5000
 
+	config.PrivValidator = MinterDir + "/config/priv_validator.json"
+
+	config.NodeKey = MinterDir + "/config/node_key.json"
+
+	config.P2P.AddrBook = MinterDir + "/config/addrbook.json"
+	config.P2P.ListenAddress = "tcp://0.0.0.0:26656"
 	return config
 }
