83e774349e96500757f8f825d05e8f3d9c742910
Daniil Lashin <danil-lashin@yandex.ru>
2018-07-20 13:40:28 +0300

    refactor genesis

--- genesis/genesis.go
+++ genesis/genesis.go
@@ -4,9 +4,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"github.com/MinterTeam/minter-go-node/core/types"
+	"github.com/MinterTeam/minter-go-node/helpers"
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/common"
 	tmtypes "github.com/tendermint/tendermint/types"
+	"math/big"
 	"time"
 )
 
@@ -18,23 +20,25 @@ func GetTestnetGenesis() *tmtypes.GenesisDoc {
 
 	appHash, _ := hex.DecodeString("0000000000000000000000000000000000000000000000000000000000000000")
 
-	appState := `{
-	"first_validator_address": "Mxa93163fdf10724dc4785ff5cbfb9ac0b5949409f",
-    "initial_balances": [
-      {
-        "address": "Mxa93163fdf10724dc4785ff5cbfb9ac0b5949409f",
-        "balance": {
-          "MNT": "10000000000000000000000000"
-        }
-      },
-      {
-        "address": "Mxfe60014a6e9ac91618f5d1cab3fd58cded61ee99",
-        "balance": {
-          "MNT": "10000000000000000000000000"
-        }
-      }
-    ]
-  }`
+	appState := AppState{
+		FirstValidatorAddress: types.HexToAddress("Mxa93163fdf10724dc4785ff5cbfb9ac0b5949409f"),
+		InitialBalances: []Account{
+			{
+				Address: types.HexToAddress("Mxa93163fdf10724dc4785ff5cbfb9ac0b5949409f"),
+				Balance: map[string]string{
+					"MNT": helpers.BipToPip(big.NewInt(100000000)).String(),
+				},
+			},
+			{
+				Address: types.HexToAddress("Mxfe60014a6e9ac91618f5d1cab3fd58cded61ee99"),
+				Balance: map[string]string{
+					"MNT": helpers.BipToPip(big.NewInt(10000000)).String(),
+				},
+			},
+		},
+	}
+
+	appStateJSON, _ := json.Marshal(appState)
 
 	genesis := tmtypes.GenesisDoc{
 		GenesisTime:     time.Date(2018, 7, 19, 0, 0, 0, 0, time.UTC),
@@ -47,7 +51,7 @@ func GetTestnetGenesis() *tmtypes.GenesisDoc {
 			},
 		},
 		AppHash:  common.HexBytes(appHash),
-		AppState: json.RawMessage([]byte(appState)),
+		AppState: json.RawMessage([]byte(appStateJSON)),
 	}
 
 	genesis.ValidateAndComplete()
