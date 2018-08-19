ad030f9f17d1b4ef6d5ebc5c65a293b630a252b1
Daniil Lashin <danil-lashin@yandex.ru>
2018-08-19 14:17:46 +0300

    bug fix

--- version/version.go
+++ version/version.go
@@ -4,12 +4,12 @@ package version
 const (
 	Maj = "0"
 	Min = "1"
-	Fix = "8"
+	Fix = "9"
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.1.8"
+	Version = "0.1.9"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
