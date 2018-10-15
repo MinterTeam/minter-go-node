e6920ec8ac1335b5041e5b55e2186a05f01e19aa
Daniil Lashin <danil-lashin@yandex.ru>
2018-10-15 12:14:12 +0300

    Add buy coin tests

--- core/transaction/buy_coin_test.go
+++ core/transaction/buy_coin_test.go
@@ -1,6 +1,9 @@
 package transaction
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/MinterTeam/minter-go-node/core/code"
 	"github.com/MinterTeam/minter-go-node/core/state"
 	"github.com/MinterTeam/minter-go-node/core/types"
 	"github.com/MinterTeam/minter-go-node/crypto"
@@ -93,5 +96,297 @@ func TestBuyCoinTx(t *testing.T) {
 	testBalance := cState.GetBalance(addr, getTestCoinSymbol())
 	if testBalance.Cmp(toBuy) != 0 {
 		t.Fatalf("Target %s balance is not correct. Expected %s, got %s", getTestCoinSymbol(), toBuy, testBalance)
+	}
+}
+
+func TestBuyCoinTxInsufficientFunds(t *testing.T) {
+	cState := getState()
+
+	createTestCoin(cState)
+
+	privateKey, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(privateKey.PublicKey)
+	coin := types.GetBaseCoin()
+
+	cState.AddBalance(addr, coin, helpers.BipToPip(big.NewInt(1)))
+
+	toBuy := helpers.BipToPip(big.NewInt(10))
+	data := BuyCoinData{
+		CoinToBuy:  getTestCoinSymbol(),
+		ValueToBuy: toBuy,
+		CoinToSell: coin,
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       coin,
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != code.InsufficientFunds {
+		t.Fatalf("Response code is not %d. Error %s", code.InsufficientFunds, response.Log)
+	}
+}
+
+func TestBuyCoinTxEqualCoins(t *testing.T) {
+	cState := getState()
+
+	privateKey, _ := crypto.GenerateKey()
+
+	data := BuyCoinData{
+		CoinToBuy:  getTestCoinSymbol(),
+		ValueToBuy: big.NewInt(0),
+		CoinToSell: getTestCoinSymbol(),
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       types.GetBaseCoin(),
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != code.CrossConvert {
+		t.Fatalf("Response code is not %d. Error %s", code.CrossConvert, response.Log)
+	}
+}
+
+func TestBuyCoinTxNotExistsBuyCoin(t *testing.T) {
+	cState := getState()
+
+	privateKey, _ := crypto.GenerateKey()
+
+	data := BuyCoinData{
+		CoinToBuy:  types.CoinSymbol{},
+		ValueToBuy: big.NewInt(0),
+		CoinToSell: types.GetBaseCoin(),
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       types.GetBaseCoin(),
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != code.CoinNotExists {
+		t.Fatalf("Response code is not %d. Error %s", code.CoinNotExists, response.Log)
+	}
+}
+
+func TestBuyCoinTxNotExistsSellCoin(t *testing.T) {
+	cState := getState()
+
+	privateKey, _ := crypto.GenerateKey()
+
+	data := BuyCoinData{
+		CoinToBuy:  types.GetBaseCoin(),
+		ValueToBuy: big.NewInt(0),
+		CoinToSell: types.CoinSymbol{},
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       types.GetBaseCoin(),
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != code.CoinNotExists {
+		t.Fatalf("Response code is not %d. Error %s", code.CoinNotExists, response.Log)
+	}
+}
+
+func TestBuyCoinTxNotExistsGasCoin(t *testing.T) {
+	cState := getState()
+
+	createTestCoin(cState)
+
+	privateKey, _ := crypto.GenerateKey()
+
+	data := BuyCoinData{
+		CoinToBuy:  types.GetBaseCoin(),
+		ValueToBuy: big.NewInt(0),
+		CoinToSell: getTestCoinSymbol(),
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       types.CoinSymbol{},
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != code.CoinNotExists {
+		t.Fatalf("Response code is not %d. Error %s", code.CoinNotExists, response.Log)
+	}
+}
+
+func TestBuyCoinTxNotGasCoin(t *testing.T) {
+	cState := getState()
+
+	createTestCoin(cState)
+
+	privateKey, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(privateKey.PublicKey)
+
+	cState.AddBalance(addr, getTestCoinSymbol(), helpers.BipToPip(big.NewInt(1000)))
+
+	data := BuyCoinData{
+		CoinToBuy:  types.GetBaseCoin(),
+		ValueToBuy: big.NewInt(1),
+		CoinToSell: getTestCoinSymbol(),
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       getTestCoinSymbol(),
+		Type:          TypeBuyCoin,
+		Data:          encodedData,
+		SignatureType: SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(tx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := RunTx(cState, false, encodedTx, big.NewInt(0), 0)
+
+	if response.Code != 0 {
+		t.Fatalf("Response code is not 0. Error %s", response.Log)
+	}
+}
+
+func TestBuyCoinTxJSON(t *testing.T) {
+	out := []byte("{\"coin_to_buy\":\"MNT\",\"value_to_buy\":\"1\",\"coin_to_sell\":\"TEST\"}")
+
+	buyCoinData := BuyCoinData{
+		CoinToBuy:  types.GetBaseCoin(),
+		ValueToBuy: big.NewInt(1),
+		CoinToSell: getTestCoinSymbol(),
+	}
+
+	result, err := json.Marshal(buyCoinData)
+
+	if err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+
+	if !bytes.Equal(out, result) {
+		t.Fatalf("Error: result is not correct %s", string(result))
 	}
 }
