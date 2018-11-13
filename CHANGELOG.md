60ad391f6a15a48c61730027aa04f3bab6fc5fc4
Daniil Lashin <danil-lashin@yandex.ru>
2018-11-13 11:37:57 +0300

    Bump versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # Changelog
 
 ## 0.6.0
+*Nov 13th, 2018*
+
+BREAKING CHANGES
+
+- [tendermint] Update to [v0.26.1](https://github.com/tendermint/tendermint/blob/master/CHANGELOG.md#v0261) 
+
+## 0.6.0
 *Oct 30th, 2018*
 
 BREAKING CHANGES
--- version/version.go
+++ version/version.go
@@ -3,13 +3,13 @@ package version
 // Version components
 const (
 	Maj = "0"
-	Min = "6"
+	Min = "7"
 	Fix = "0"
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.6.0"
+	Version = "0.7.0"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
