dba7652c4a33aa626d3f96c0f95912dbbdb71eee
Daniil Lashin <danil-lashin@yandex.ru>
2018-11-15 15:28:23 +0300

    Update genesis

--- genesis/genesis.go
+++ genesis/genesis.go
@@ -12,8 +12,8 @@ import (
 )
 
 var (
-	Network     = "minter-test-network-25"
-	genesisTime = time.Date(2018, 11, 15, 15, 0, 0, 0, time.UTC)
+	Network     = "minter-test-network-26"
+	genesisTime = time.Date(2018, 11, 16, 15, 0, 0, 0, time.UTC)
 )
 
 func GetTestnetGenesis() (*tmtypes.GenesisDoc, error) {
