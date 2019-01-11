55b5bbc6969e65e72d9bfd4e906145e8d5ff9232
Daniil Lashin <danil-lashin@yandex.ru>
2019-01-11 18:01:04 +0300

    Add tests

--- core/minter/minter_test.go
+++ core/minter/minter_test.go
@@ -2,13 +2,19 @@ package minter
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"github.com/MinterTeam/minter-go-node/cmd/utils"
 	"github.com/MinterTeam/minter-go-node/config"
+	"github.com/MinterTeam/minter-go-node/core/transaction"
 	"github.com/MinterTeam/minter-go-node/core/types"
+	"github.com/MinterTeam/minter-go-node/crypto"
 	"github.com/MinterTeam/minter-go-node/genesis"
 	"github.com/MinterTeam/minter-go-node/helpers"
 	"github.com/MinterTeam/minter-go-node/log"
+	"github.com/MinterTeam/minter-go-node/rlp"
 	tmConfig "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	tmNode "github.com/tendermint/tendermint/node"
@@ -27,6 +33,8 @@ import (
 
 var pv *privval.FilePV
 var cfg *tmConfig.Config
+var client *rpc.Local
+var privateKey *ecdsa.PrivateKey
 
 func init() {
 	*utils.MinterHome = os.ExpandEnv(filepath.Join("$HOME", ".minter_test"))
@@ -36,11 +44,11 @@ func init() {
 
 	pv = privval.GenFilePV(cfg.PrivValidatorFile())
 	pv.Save()
-}
 
-func TestMinterBlockchain(t *testing.T) {
+	b, _ := hex.DecodeString("825ca965c34ef1c8343e8e377959108370c23ba6194d858452b63432456403f9")
+	privateKey, _ = crypto.ToECDSA(b)
+
 	app := NewMinterBlockchain()
-
 	nodeKey, err := p2p.LoadOrGenNodeKey(cfg.NodeKeyFile())
 	if err != nil {
 		panic(err)
@@ -58,22 +66,22 @@ func TestMinterBlockchain(t *testing.T) {
 	)
 
 	if err != nil {
-		t.Fatalf("Failed to create a node: %v", err)
+		panic(fmt.Sprintf("Failed to create a node: %v", err))
 	}
 
 	if err = node.Start(); err != nil {
-		t.Fatalf("Failed to start node: %v", err)
+		panic(fmt.Sprintf("Failed to start node: %v", err))
 	}
 
 	log.Info("Started node", "nodeInfo", node.Switch().NodeInfo())
 	app.SetTmNode(node)
-	client := rpc.NewLocal(node)
+	client = rpc.NewLocal(node)
+}
 
+func TestBlocksCreation(t *testing.T) {
 	// Wait for blocks
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
 	blocks := make(chan interface{})
-	err = client.Subscribe(ctx, "test-client", query.MustParse("tm.event = 'NewBlock'"), blocks)
+	err := client.Subscribe(context.TODO(), "test-client", query.MustParse("tm.event = 'NewBlock'"), blocks)
 	if err != nil {
 		panic(err)
 	}
@@ -85,11 +93,72 @@ func TestMinterBlockchain(t *testing.T) {
 		t.Fatalf("Timeout waiting for the first block")
 	}
 
-	err = node.Stop()
+	err = client.UnsubscribeAll(context.TODO(), "test-client")
 	if err != nil {
 		panic(err)
 	}
-	app.Stop()
+}
+
+func TestSendTx(t *testing.T) {
+	for blockchain.Height() < 2 {
+		time.Sleep(5 * time.Second)
+	}
+
+	value := helpers.BipToPip(big.NewInt(10))
+	to := types.Address([20]byte{1})
+
+	data := transaction.SendData{
+		Coin:  types.GetBaseCoin(),
+		To:    to,
+		Value: value,
+	}
+
+	encodedData, err := rlp.EncodeToBytes(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := transaction.Transaction{
+		Nonce:         1,
+		GasPrice:      big.NewInt(1),
+		GasCoin:       types.GetBaseCoin(),
+		Type:          transaction.TypeSend,
+		Data:          encodedData,
+		SignatureType: transaction.SigTypeSingle,
+	}
+
+	if err := tx.Sign(privateKey); err != nil {
+		t.Fatal(err)
+	}
+
+	txBytes, _ := tx.Serialize()
+
+	res, err := client.BroadcastTxSync(txBytes)
+	if err != nil {
+		t.Fatalf("Failed: %s", err.Error())
+	}
+
+	if res.Code != 0 {
+		t.Fatalf("CheckTx code is not 0: %d", res.Code)
+	}
+
+	txs := make(chan interface{})
+	err = client.Subscribe(context.TODO(), "test-client", query.MustParse(fmt.Sprintf("tm.event = 'Tx'")), txs)
+	if err != nil {
+		panic(err)
+	}
+
+	select {
+	case <-txs:
+		// got tx
+	case <-time.After(10 * time.Second):
+		t.Fatalf("Timeout waiting for the tx to be committed")
+	}
+
+	err = client.UnsubscribeAll(context.TODO(), "test-client")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getGenesis() (*types2.GenesisDoc, error) {
@@ -103,16 +172,10 @@ func getGenesis() (*types2.GenesisDoc, error) {
 	appHash := [32]byte{}
 
 	appState := genesis.AppState{
-		FirstValidatorAddress: types.HexToAddress("Mxee81347211c72524338f9680072af90744333146"),
+		FirstValidatorAddress: crypto.PubkeyToAddress(privateKey.PublicKey),
 		InitialBalances: []genesis.Account{
 			{
-				Address: types.HexToAddress("Mxee81347211c72524338f9680072af90744333146"),
-				Balance: map[string]string{
-					"MNT": helpers.BipToPip(big.NewInt(100000000)).String(),
-				},
-			},
-			{
-				Address: types.HexToAddress("Mx184ac726059e43643e67290666f7b3195093f870"),
+				Address: crypto.PubkeyToAddress(privateKey.PublicKey),
 				Balance: map[string]string{
 					"MNT": helpers.BipToPip(big.NewInt(100000000)).String(),
 				},
