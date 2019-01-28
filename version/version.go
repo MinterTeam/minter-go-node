8e94dd18e49a5dc65bba09717588226800b6eb07
Daniil Lashin <danil-lashin@yandex.ru>
2019-01-28 18:21:40 +0300

    Update version

--- version/version.go
+++ version/version.go
@@ -3,15 +3,15 @@ package version
 // Version components
 const (
 	Maj = "0"
-	Min = "10"
-	Fix = "1"
+	Min = "11"
+	Fix = "0"
 
 	AppVer = 2
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.10.1"
+	Version = "0.11.0"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
