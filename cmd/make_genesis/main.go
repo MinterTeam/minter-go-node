7b44efe5ed46df6ac1e24f09f4b9a12fae48e571
Daniil Lashin <danil-lashin@yandex.ru>
2019-04-05 15:24:23 +0300

    Update genesis

--- cmd/make_genesis/main.go
+++ cmd/make_genesis/main.go
@@ -25,18 +25,18 @@ func main() {
 	}
 
 	balances := map[string]int64{
-		"Mx50003880d87db2fa48f6b824cdcfeeab1ac77733": 15021622, // DeCenter
-		"Mxe6732a97c6445edb0becf685dd92655bb4a1b838": 12644321, // Minter One
-		"Mx89f5395a03847826d6b48bb02dbde64376945a20": 5038353,  // MonsterNode
-		"Mx8da4f97b635cf751f2c0f0020f2e78ceb691d7d5": 3434147,  // BTC.Secure
+		"Mxe6732a97c6445edb0becf685dd92655bb4a1b838": 17318590, // Minter One
+		"Mx89f5395a03847826d6b48bb02dbde64376945a20": 9833248,  // MonsterNode
+		"Mx8da4f97b635cf751f2c0f0020f2e78ceb691d7d5": 3798414,  // BTC.Secure
+		"Mxf5d006607e9420978b8f33a940a6fcbc67797db2": 2187460,  // Minter Store
+		"Mx50003880d87db2fa48f6b824cdcfeeab1ac77733": 2038305, // DeCenter
 		"Mx198208eb4d11d4b389ff262ac52494d920770879": 1063320,  // MINTER CENTER
-		"Mxf5d006607e9420978b8f33a940a6fcbc67797db2": 1003783,  // Minter Store
-		"Mx8bee92ba8999ab047cb5e2d98e190dada4d7a2b2": 813816,   // StakeHolder
 		"Mx3bdee0d64fa9ac892720f48724ef6a4e2919a6ba": 803817,   // Minternator
+		"Mx8bee92ba8999ab047cb5e2d98e190dada4d7a2b2": 803118,   // StakeHolder
 		"Mxe1dbde5c02a730f747a47d24f0f993c27da9dff1": 636837,   // Rundax
-		"Mx601609b85ee21b9493dffbca1079c74d47b75f2a": 165133,   // PRO-BLOCKCHAIN
+		"Mx601609b85ee21b9493dffbca1079c74d47b75f2a": 158857,   // PRO-BLOCKCHAIN
 		"Mxdce154b6e1d06b46e95881b900eeb164e247c180": 75414,    // Mother Minter
-		"Mxdc7fcc63930bf81ebdce12b3bcef57b93e99a157": 61982,    // Validator.Center
+		"Mxdc7fcc63930bf81ebdce12b3bcef57b93e99a157": 68258,    // Validator.Center
 		"Mx0acbd5df9bc4bdc9fcf2f87e8393907739401a27": 34512,    // bipMaker
 		"Mx35c40563ee5181899d0d605839edb9e940b0d8e5": 33869,    // SolidMinter
 	}
