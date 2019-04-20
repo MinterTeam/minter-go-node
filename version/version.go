a9927e34746946457471730f76b5e5dc1aad19b0
Daniil Lashin <danil-lashin@yandex.ru>
2019-04-20 10:19:30 +0300

    Fix set candidate remainder issue

--- version/version.go
+++ version/version.go
@@ -4,14 +4,14 @@ package version
 const (
 	Maj = "0"
 	Min = "19"
-	Fix = "2"
+	Fix = "3"
 
 	AppVer = 4
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.19.2"
+	Version = "0.19.3"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
