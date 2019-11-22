9f0387b6b7af64c3b4a4ec69f73832361481d758
Klimov Sergey <klim0v-sergey@yandex.ru>
2019-11-22 12:09:54 +0300

    update manager (#267)

--- cmd/minter/cmd/manager.go
+++ cmd/minter/cmd/manager.go
@@ -6,12 +6,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Manager = &cobra.Command{
+var ManagerCommand = &cobra.Command{
 	Use:                "manager",
 	Short:              "Minter CLI manager",
 	DisableFlagParsing: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		service.RunCli(utils.GetMinterHome()+"/manager.sock", args)
+		console, err := service.ConfigureManagerConsole(utils.GetMinterHome() + "/manager.sock")
+		if err != nil {
+			return nil
+		}
+		return console.Execute(args)
+	},
+}
+
+var ManagerConsole = &cobra.Command{
+	Use:                "console",
+	Short:              "Minter CLI manager",
+	DisableFlagParsing: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		console, err := service.ConfigureManagerConsole(utils.GetMinterHome() + "/manager.sock")
+		if err != nil {
+			return nil
+		}
+		console.Cli()
 		return nil
 	},
 }
