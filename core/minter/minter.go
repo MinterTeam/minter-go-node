05d7f2eef22fe95bdac8abbb343c560c057cc6f8
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2019-12-20 14:25:14 +0300

    Fix

--- core/minter/minter.go
+++ core/minter/minter.go
@@ -389,7 +389,7 @@ func (app *Blockchain) CheckTx(req abciTypes.RequestCheckTx) abciTypes.ResponseC
 
 // Commit the state and return the application Merkle root hash
 func (app *Blockchain) Commit() abciTypes.ResponseCommit {
-	if app.height > 1 {
+	if app.height > app.appDB.GetStartHeight() + 1 {
 		if err := app.stateDeliver.Check(); err != nil {
 			panic(err)
 		}
