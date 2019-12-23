f863b77854aabdb43c6462cbf6166dfb717e3a6b
klim0v <klim0v-sergey@yandex.ru>
2019-12-23 22:49:37 +0300

    wip: handle errors

--- api/v2/service/send_transaction.go
+++ api/v2/service/send_transaction.go
@@ -1,12 +1,9 @@
 package service
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/MinterTeam/minter-go-node/api/v2/pb"
-	"github.com/golang/protobuf/jsonpb"
 	_struct "github.com/golang/protobuf/ptypes/struct"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -27,29 +24,10 @@ func (s *Service) SendTransaction(_ context.Context, req *pb.SendTransactionRequ
 	}
 
 	if result.Code != 0 {
-		statusErr := status.New(codes.InvalidArgument, result.Log)
-		detailsMap := &_struct.Struct{Fields: make(map[string]*_struct.Value)}
-		marshal, err := json.Marshal(map[string]string{
+		return new(pb.SendTransactionResponse), s.createError(status.New(codes.InvalidArgument, result.Log), map[string]string{
 			"code": fmt.Sprintf("%d", result.Code),
 			"log":  result.Log,
 		})
-		if err != nil {
-			s.client.Logger.Error(err.Error())
-			return new(pb.SendTransactionResponse), statusErr.Err()
-		}
-
-		var bb bytes.Buffer
-		bb.Write(marshal)
-		if err := (&jsonpb.Unmarshaler{}).Unmarshal(&bb, detailsMap); err != nil {
-			return nil, err
-		}
-
-		withDetails, err := statusErr.WithDetails(detailsMap)
-		if err != nil {
-			s.client.Logger.Error(err.Error())
-			return new(pb.SendTransactionResponse), statusErr.Err()
-		}
-		return &pb.SendTransactionResponse{}, withDetails.Err()
 	}
 
 	return &pb.SendTransactionResponse{
--- api/v2/service/service.go
+++ api/v2/service/service.go
@@ -1,11 +1,16 @@
 package service
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/MinterTeam/minter-go-node/config"
 	"github.com/MinterTeam/minter-go-node/core/minter"
 	"github.com/MinterTeam/minter-go-node/core/state"
+	"github.com/golang/protobuf/jsonpb"
+	_struct "github.com/golang/protobuf/ptypes/struct"
 	"github.com/tendermint/go-amino"
 	rpc "github.com/tendermint/tendermint/rpc/client"
+	"google.golang.org/grpc/status"
 )
 
 type Service struct {
@@ -30,4 +35,32 @@ func (s *Service) getStateForHeight(height int32) (*state.State, error) {
 	}
 
 	return s.blockchain.CurrentState(), nil
+}
+
+func (s *Service) createError(statusErr *status.Status, details map[string]string) error {
+	if details == nil {
+		return statusErr.Err()
+	}
+
+	detailsMap := &_struct.Struct{Fields: make(map[string]*_struct.Value)}
+	marshal, err := json.Marshal(details)
+	if err != nil {
+		s.client.Logger.Error(err.Error())
+		return statusErr.Err()
+	}
+
+	var bb bytes.Buffer
+	bb.Write(marshal)
+	if err := (&jsonpb.Unmarshaler{}).Unmarshal(&bb, detailsMap); err != nil {
+		s.client.Logger.Error(err.Error())
+		return statusErr.Err()
+	}
+
+	withDetails, err := statusErr.WithDetails(detailsMap)
+	if err != nil {
+		s.client.Logger.Error(err.Error())
+		return statusErr.Err()
+	}
+
+	return withDetails.Err()
 }
