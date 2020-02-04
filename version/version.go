2a4b585f2ba1fc39eb6b1f30c83fcbc264585344
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-02-04 12:25:46 +0300

    Update version

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const (
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "1.1.0-beta7"
+	Version = "1.1.0-beta8"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
