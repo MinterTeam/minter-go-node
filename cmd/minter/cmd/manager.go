d629d7d8dc1dbe345d4d1616cb02a5eceb5c0b33
Klimov Sergey <klim0v-sergey@yandex.ru>
2020-02-14 16:37:56 +0300

    fix flag error in cli command

--- cmd/minter/cmd/manager.go
+++ cmd/minter/cmd/manager.go
@@ -4,6 +4,7 @@ import (
 	"github.com/MinterTeam/minter-go-node/cli/service"
 	"github.com/MinterTeam/minter-go-node/cmd/utils"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 var ManagerCommand = &cobra.Command{
@@ -11,11 +12,12 @@ var ManagerCommand = &cobra.Command{
 	Short:              "Minter manager execute command",
 	DisableFlagParsing: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		newArgs := setParentFlags(cmd,args)
 		console, err := service.ConfigureManagerConsole(utils.GetMinterHome() + "/manager.sock")
 		if err != nil {
 			return nil
 		}
-		return console.Execute(args)
+		return console.Execute(newArgs)
 	},
 }
 
@@ -24,6 +26,7 @@ var ManagerConsole = &cobra.Command{
 	Short:              "Minter CLI manager",
 	DisableFlagParsing: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		_ = setParentFlags(cmd,args)
 		console, err := service.ConfigureManagerConsole(utils.GetMinterHome() + "/manager.sock")
 		if err != nil {
 			return nil
@@ -31,4 +34,18 @@ var ManagerConsole = &cobra.Command{
 		console.Cli()
 		return nil
 	},
+}
+
+func setParentFlags(cmd *cobra.Command, args []string) (newArgs []string){
+	for _, arg := range args {
+		split := strings.Split(arg, "=")
+		if len(split) == 2 {
+			err := cmd.Parent().PersistentFlags().Set(strings.TrimLeft(split[0], "-"), split[1])
+			if err == nil {
+				continue
+			}
+		}
+		newArgs = append(newArgs, arg)
+	}
+	return newArgs
 }
