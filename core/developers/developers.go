86246f46595d1975584091514a2eb8be1db85a9e
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-02-28 01:37:57 +0300

    Change developers address

--- core/developers/developers.go
+++ core/developers/developers.go
@@ -3,6 +3,6 @@ package developers
 import "github.com/MinterTeam/minter-go-node/core/types"
 
 var (
-	Address    = types.HexToAddress("Mx04bea23efb744dc93b4fda4c20bf4a21c6e195f1")
+	Address    = types.HexToAddress("Mx07706fac38854036d2ba077ac3769064bab7082f")
 	Commission = 10
 )
