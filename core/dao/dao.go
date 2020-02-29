4d4644870ba2beb16d136aaf6acd06aedd484bf3
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-02-29 19:15:15 +0300

    Update DAO address

--- core/dao/dao.go
+++ core/dao/dao.go
@@ -3,6 +3,6 @@ package dao
 import "github.com/MinterTeam/minter-go-node/core/types"
 
 var (
-	Address    = types.HexToAddress("Mx18467bbb64a8edf890201d526c35957d82be3d95")
+	Address    = types.HexToAddress("Mx3339b3287a21b7d58895876d6409418cf1fdb666")
 	Commission = 10
 )
