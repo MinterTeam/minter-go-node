81716774e91e80d55b12a331bcc301ccee630d75
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-09-02 09:16:01 +0300

    set testnet flag

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const (
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "1.2.0"
+	Version = "1.2.0-testnet"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
