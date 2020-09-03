509073dde91fb2680144c9739ecb730a56443092
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-09-03 11:56:20 +0300

    Fix linter errors on appdb

--- core/appdb/appdb.go
+++ core/appdb/appdb.go
@@ -24,14 +24,19 @@ const (
 	dbName = "app"
 )
 
+// AppDB is responsible for storing basic information about app state on disk
 type AppDB struct {
 	db db.DB
 }
 
+// Close closes db connection, panics on error
 func (appDB *AppDB) Close() {
-	appDB.db.Close()
+	if err := appDB.db.Close(); err != nil {
+		panic(err)
+	}
 }
 
+// GetLastBlockHash returns latest block hash stored on disk
 func (appDB *AppDB) GetLastBlockHash() []byte {
 	var hash [32]byte
 
@@ -44,12 +49,14 @@ func (appDB *AppDB) GetLastBlockHash() []byte {
 	return hash[:]
 }
 
+// SetLastBlockHash stores given block hash on disk, panics on error
 func (appDB *AppDB) SetLastBlockHash(hash []byte) {
 	if err := appDB.db.Set([]byte(hashPath), hash); err != nil {
 		panic(err)
 	}
 }
 
+// GetLastHeight returns latest block height stored on disk
 func (appDB *AppDB) GetLastHeight() uint64 {
 	result, err := appDB.db.Get([]byte(heightPath))
 	if err != nil {
@@ -64,6 +71,7 @@ func (appDB *AppDB) GetLastHeight() uint64 {
 	return height
 }
 
+// SetLastHeight stores given block height on disk, panics on error
 func (appDB *AppDB) SetLastHeight(height uint64) {
 	h := make([]byte, 8)
 	binary.BigEndian.PutUint64(h, height)
@@ -72,12 +80,16 @@ func (appDB *AppDB) SetLastHeight(height uint64) {
 	}
 }
 
+// SetStartHeight stores given block height on disk as start height, panics on error
 func (appDB *AppDB) SetStartHeight(height uint64) {
 	h := make([]byte, 8)
 	binary.BigEndian.PutUint64(h, height)
-	appDB.db.Set([]byte(startHeightPath), h)
+	if err := appDB.db.Set([]byte(startHeightPath), h); err != nil {
+		panic(err)
+	}
 }
 
+// GetStartHeight returns start height stored on disk
 func (appDB *AppDB) GetStartHeight() uint64 {
 	result, err := appDB.db.Get([]byte(startHeightPath))
 	if err != nil {
@@ -92,6 +104,7 @@ func (appDB *AppDB) GetStartHeight() uint64 {
 	return height
 }
 
+// GetValidators returns list of latest validators stored on dist
 func (appDB *AppDB) GetValidators() types.ValidatorUpdates {
 	result, err := appDB.db.Get([]byte(validatorsPath))
 	if err != nil {
@@ -105,7 +118,6 @@ func (appDB *AppDB) GetValidators() types.ValidatorUpdates {
 	var vals types.ValidatorUpdates
 
 	err = cdc.UnmarshalBinaryBare(result, &vals)
-
 	if err != nil {
 		panic(err)
 	}
@@ -113,45 +125,49 @@ func (appDB *AppDB) GetValidators() types.ValidatorUpdates {
 	return vals
 }
 
+// SaveValidators stores given validators list on disk, panics on error
 func (appDB *AppDB) SaveValidators(vals types.ValidatorUpdates) {
 	data, err := cdc.MarshalBinaryBare(vals)
-
 	if err != nil {
 		panic(err)
 	}
 
-	appDB.db.Set([]byte(validatorsPath), data)
+	if err := appDB.db.Set([]byte(validatorsPath), data); err != nil {
+		panic(err)
+	}
 }
 
-type LastBlocksTimeDelta struct {
+type lastBlocksTimeDelta struct {
 	Height uint64
 	Delta  int
 }
 
+// GetLastBlocksTimeDelta returns delta of time between latest blocks
 func (appDB *AppDB) GetLastBlocksTimeDelta(height uint64) (int, error) {
 	result, err := appDB.db.Get([]byte(blockTimeDeltaPath))
 	if err != nil {
 		panic(err)
 	}
 	if result == nil {
-		return 0, errors.New("no info about LastBlocksTimeDelta is available")
+		return 0, errors.New("no info about lastBlocksTimeDelta is available")
 	}
 
-	data := LastBlocksTimeDelta{}
+	data := lastBlocksTimeDelta{}
 	err = cdc.UnmarshalBinaryBare(result, &data)
 	if err != nil {
 		panic(err)
 	}
 
 	if data.Height != height {
-		return 0, errors.New("no info about LastBlocksTimeDelta is available")
+		return 0, errors.New("no info about lastBlocksTimeDelta is available")
 	}
 
 	return data.Delta, nil
 }
 
+// SetLastBlocksTimeDelta stores delta of time between latest blocks
 func (appDB *AppDB) SetLastBlocksTimeDelta(height uint64, delta int) {
-	data, err := cdc.MarshalBinaryBare(LastBlocksTimeDelta{
+	data, err := cdc.MarshalBinaryBare(lastBlocksTimeDelta{
 		Height: height,
 		Delta:  delta,
 	})
@@ -160,9 +176,12 @@ func (appDB *AppDB) SetLastBlocksTimeDelta(height uint64, delta int) {
 		panic(err)
 	}
 
-	appDB.db.Set([]byte(blockTimeDeltaPath), data)
+	if err := appDB.db.Set([]byte(blockTimeDeltaPath), data); err != nil {
+		panic(err)
+	}
 }
 
+// NewAppDB creates AppDB instance with given config
 func NewAppDB(cfg *config.Config) *AppDB {
 	return &AppDB{
 		db: db.NewDB(dbName, db.BackendType(cfg.DBBackend), utils.GetMinterHome()+"/data"),
