a71963348d32d67023c4bf65cb6852fbc29e3815
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-09-03 13:13:06 +0300

    Fix linter errors

--- core/developers/developers.go
+++ core/developers/developers.go
@@ -1,15 +1,11 @@
 package developers
 
 import (
-	"github.com/MinterTeam/minter-go-node/core/state/accounts"
 	"github.com/MinterTeam/minter-go-node/core/types"
 )
 
+// Commission which is subtracted from rewards and being send to Developers Address
 var (
-	Address = (&accounts.Multisig{
-		Threshold: 2,
-		Weights:   []uint{1, 1, 1},
-		Addresses: []types.Address{types.HexToAddress("Mx22df0f98c1b421974fb0c64440258aaffd4e96d8"), types.HexToAddress("Mxf8821646818a873e3efac40cc2c13f96e3515aa1"), types.HexToAddress("Mx90a82ed6fd69cdd125474ce9349a8e34fb4f5ffe")},
-	}).Address()
-	Commission = 10
+	Address    = types.HexToAddress("Mx688568d9d70c57e71d0b9de6480afb0d317f885c")
+	Commission = 10 // in %
 )
