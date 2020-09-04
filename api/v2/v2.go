7e092e87a96942a4ce5acbe11ce54c22065dae45
klim0v <klim0v-sergey@yandex.ru>
2020-09-04 14:24:00 +0300

    delete code from data error

--- api/v2/v2.go
+++ api/v2/v2.go
@@ -167,6 +167,9 @@ func httpError(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Mar
 	w.WriteHeader(st)
 
 	codeString, data := parseStatus(s)
+	if _, ok := data["code"]; ok {
+		delete(data, "code")
+	}
 
 	jErr := json.NewEncoder(w).Encode(gw.ErrorBody{
 		Error: &gw.ErrorBody_Error{
