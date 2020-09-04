173ead448c733a1b681a0792ec5e1ea9455a1868
klim0v <klim0v-sergey@yandex.ru>
2020-09-04 13:48:32 +0300

    api

--- api/v2/service/estimate_tx_commission.go
+++ api/v2/service/estimate_tx_commission.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"github.com/MinterTeam/minter-go-node/core/commissions"
-	"github.com/MinterTeam/minter-go-node/core/state"
 	"github.com/MinterTeam/minter-go-node/core/transaction"
-	"github.com/MinterTeam/minter-go-node/core/types"
 	"github.com/MinterTeam/minter-go-node/formula"
 	pb "github.com/MinterTeam/node-grpc-gateway/api_pb"
 	"google.golang.org/grpc/codes"
@@ -24,15 +21,6 @@ func (s *Service) EstimateTxCommission(ctx context.Context, req *pb.EstimateTxCo
 
 	cState.RLock()
 	defer cState.RUnlock()
-
-	if req.GetTx() == "" {
-		data := req.GetData()
-		if data == nil {
-			return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "invalid tx and data")
-		}
-
-		return commissionCoinForData(data, cState)
-	}
 
 	if !strings.HasPrefix(strings.Title(req.GetTx()), "0x") {
 		return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "invalid transaction")
@@ -69,66 +57,4 @@ func (s *Service) EstimateTxCommission(ctx context.Context, req *pb.EstimateTxCo
 	return &pb.EstimateTxCommissionResponse{
 		Commission: commission.String(),
 	}, nil
-}
-
-func commissionCoinForData(data *pb.EstimateTxCommissionRequest_TransactionData, cState *state.CheckState) (*pb.EstimateTxCommissionResponse, error) {
-	var commissionInBaseCoin *big.Int
-	switch data.Type {
-	case pb.EstimateTxCommissionRequest_TransactionData_Send:
-		commissionInBaseCoin = big.NewInt(commissions.SendTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_SellAllCoin,
-		pb.EstimateTxCommissionRequest_TransactionData_SellCoin,
-		pb.EstimateTxCommissionRequest_TransactionData_BuyCoin:
-		commissionInBaseCoin = big.NewInt(commissions.ConvertTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_DeclareCandidacy:
-		commissionInBaseCoin = big.NewInt(commissions.DeclareCandidacyTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_Delegate:
-		commissionInBaseCoin = big.NewInt(commissions.DelegateTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_Unbond:
-		commissionInBaseCoin = big.NewInt(commissions.UnbondTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_SetCandidateOffline,
-		pb.EstimateTxCommissionRequest_TransactionData_SetCandidateOnline:
-		commissionInBaseCoin = big.NewInt(commissions.ToggleCandidateStatus)
-	case pb.EstimateTxCommissionRequest_TransactionData_EditCandidate:
-		commissionInBaseCoin = big.NewInt(commissions.EditCandidate)
-	case pb.EstimateTxCommissionRequest_TransactionData_RedeemCheck:
-		commissionInBaseCoin = big.NewInt(commissions.RedeemCheckTx)
-	case pb.EstimateTxCommissionRequest_TransactionData_CreateMultisig:
-		commissionInBaseCoin = big.NewInt(commissions.CreateMultisig)
-	case pb.EstimateTxCommissionRequest_TransactionData_Multisend:
-		if data.Mtxs <= 0 {
-			return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "Set number of transactions for multisend (mtxs)")
-		}
-		commissionInBaseCoin = big.NewInt(commissions.MultisendDelta*(data.Mtxs-1) + 10)
-	default:
-		return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "Set correct transaction type for tx")
-	}
-
-	lenPayload := len(data.Payload)
-	if lenPayload > 1024 {
-		return new(pb.EstimateTxCommissionResponse), status.Errorf(codes.InvalidArgument, "Transaction payload length is over %d bytes", 1024)
-	}
-
-	totalCommissionInBaseCoin := new(big.Int).Mul(big.NewInt(0).Add(commissionInBaseCoin, big.NewInt(int64(lenPayload))), transaction.CommissionMultiplier)
-
-	if types.CoinID(data.GasCoinId).IsBaseCoin() {
-		return &pb.EstimateTxCommissionResponse{
-			Commission: totalCommissionInBaseCoin.String(),
-		}, nil
-	}
-
-	coin := cState.Coins().GetCoin(types.CoinID(data.GasCoinId))
-
-	if coin == nil {
-		return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "Gas Coin not found")
-	}
-
-	if totalCommissionInBaseCoin.Cmp(coin.Reserve()) == 1 {
-		return new(pb.EstimateTxCommissionResponse), status.Error(codes.InvalidArgument, "Not enough coin reserve for pay comission")
-	}
-
-	return &pb.EstimateTxCommissionResponse{
-		Commission: formula.CalculateSaleAmount(coin.Volume(), coin.Reserve(), coin.Crr(), totalCommissionInBaseCoin).String(),
-	}, nil
-
 }
