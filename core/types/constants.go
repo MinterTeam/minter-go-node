bfd17f660cbf6cbb3ee4bf5fddc9ef9cebac0848
klim0v <klim0v-sergey@yandex.ru>
2020-09-07 16:17:30 +0300

    add --testnet flag

--- core/types/constants.go
+++ core/types/constants.go
@@ -1,23 +1,29 @@
 package types
 
+// ChainID is ID of the network (1 - mainnet, 2 - testnet)
 type ChainID byte
 
 const (
-	ChainTestnet = 0x02
-	ChainMainnet = 0x01
-
-	CurrentChainID = ChainTestnet
+	// ChainMainnet is mainnet chain ID of the network
+	ChainMainnet ChainID = 0x01
+	// ChainTestnet is mainnet chain ID of the network
+	ChainTestnet ChainID = 0x02
 )
+
+// CurrentChainID is current ChainID of the network
+var CurrentChainID = ChainMainnet
 
 var (
 	coinTestnet = StrToCoinSymbol("MNT")
 	coinMainnet = StrToCoinSymbol("BIP")
 )
 
+// GetBaseCoin returns the coin symbol of the current ChainID
 func GetBaseCoin() CoinSymbol {
 	return getBaseCoin(CurrentChainID)
 }
 
+// GetBaseCoinID returns ID of base coin
 func GetBaseCoinID() CoinID {
 	return BasecoinID
 }
