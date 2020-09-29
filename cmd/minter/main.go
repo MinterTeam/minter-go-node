b625f1af9f8c986ca0f33fa162ef7b333f38c301
klim0v <klim0v-sergey@yandex.ru>
2020-09-29 13:46:48 +0300

    add intent and start-height to export cli

--- cmd/minter/main.go
+++ cmd/minter/main.go
@@ -36,6 +36,8 @@ func main() {
 	rootCmd.PersistentFlags().String("pprof-addr", "0.0.0.0:6060", "pprof listen addr")
 
 	cmd.ExportCommand.Flags().Uint64("height", 0, "export height")
+	cmd.ExportCommand.Flags().Uint64("start-height", 0, "height for starting a new chain")
+	cmd.ExportCommand.Flags().Bool("indent", false, "using indent")
 	cmd.ExportCommand.Flags().String("chain_id", "", "export chain id")
 	cmd.ExportCommand.Flags().Duration("genesis_time", 0, "export height")
 
