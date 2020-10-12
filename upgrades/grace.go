3310d48bd3e284fd90cbe8434e96b2d823b9b44e
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2020-10-12 15:32:46 +0300

    Add grace period

---
+++ upgrades/grace.go
@@ -0,0 +1,28 @@
+package upgrades
+
+var gracePeriods = []*gracePeriod{
+	newGracePeriod(1, 120),
+}
+
+func IsGraceBlock(block uint64) bool {
+	for _, gp := range gracePeriods {
+		if gp.isApplicable(block) {
+			return true
+		}
+	}
+
+	return false
+}
+
+type gracePeriod struct {
+	from uint64
+	to   uint64
+}
+
+func (gp *gracePeriod) isApplicable(block uint64) bool {
+	return block >= gp.from && block <= gp.to
+}
+
+func newGracePeriod(from uint64, to uint64) *gracePeriod {
+	return &gracePeriod{from: from, to: to}
+}
