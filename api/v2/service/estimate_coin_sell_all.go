1f6aa64f01cbab79e9ca779c8bc8e48ba184026f
klim0v <klim0v-sergey@yandex.ru>
2020-12-10 22:59:49 +0300

    fix estimates

--- api/v2/service/estimate_coin_sell_all.go
+++ api/v2/service/estimate_coin_sell_all.go
@@ -80,10 +80,10 @@ func (s *Service) EstimateCoinSellAll(ctx context.Context, req *pb.EstimateCoinS
 		}
 	}
 	value.Sub(value, commissionInBaseCoin)
+	if value.Sign() != 1 {
+		return nil, status.New(codes.FailedPrecondition, "Not enough coins to pay commission").Err()
+	}
 	if !coinToBuy.IsBaseCoin() {
-		if value.Sign() != 1 {
-			return nil, status.New(codes.FailedPrecondition, "Not enough coins to pay commission").Err()
-		}
 		value = formula.CalculatePurchaseReturn(coinTo.Volume(), coinTo.Reserve(), coinTo.Crr(), value)
 		if errResp := transaction.CheckForCoinSupplyOverflow(coinTo, value); errResp != nil {
 			return nil, s.createError(status.New(codes.FailedPrecondition, errResp.Log), errResp.Info)
