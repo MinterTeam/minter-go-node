896280ea9697f8703c08228c776dceef7e50d5ee
klim0v <klim0v-sergey@yandex.ru>
2020-12-10 13:51:13 +0300

    optimize account import

--- core/state/accounts/accounts.go
+++ core/state/accounts/accounts.go
@@ -333,50 +333,54 @@ func (a *Accounts) markDirty(addr types.Address) {
 }
 
 func (a *Accounts) Export(state *types.AppState) {
-	// todo: iterate range?
-	a.immutableTree().Iterate(func(key []byte, value []byte) bool {
-		if key[0] == mainPrefix {
-			addressPath := key[1:]
-			if len(addressPath) > types.AddressLength {
-				return false
-			}
-
-			address := types.BytesToAddress(addressPath)
-			account := a.get(address)
-
-			var balance []types.Balance
-			for _, b := range a.GetBalances(account.address) {
-				balance = append(balance, types.Balance{
-					Coin:  uint64(b.Coin.ID),
-					Value: b.Value.String(),
-				})
-			}
-
-			// sort balances by coin symbol
-			sort.SliceStable(balance, func(i, j int) bool {
-				return bytes.Compare(types.CoinID(balance[i].Coin).Bytes(), types.CoinID(balance[j].Coin).Bytes()) == 1
-			})
-
-			acc := types.Account{
-				Address: account.address,
-				Balance: balance,
-				Nonce:   account.Nonce,
-			}
-
-			if account.IsMultisig() {
-				var weights []uint64
-				for _, weight := range account.MultisigData.Weights {
-					weights = append(weights, uint64(weight))
-				}
-				acc.MultisigData = &types.Multisig{
-					Weights:   weights,
-					Threshold: uint64(account.MultisigData.Threshold),
-					Addresses: account.MultisigData.Addresses,
-				}
-			}
-
-			state.Accounts = append(state.Accounts, acc)
+	a.immutableTree().IterateRange([]byte{mainPrefix}, []byte{mainPrefix + 1}, true, func(key []byte, value []byte) bool {
+		addressPath := key[1:]
+		if len(addressPath) > types.AddressLength {
+			return false
 		}
+
+		address := types.BytesToAddress(addressPath)
+		account := a.get(address)
+
+		var balance []types.Balance
+		for _, b := range a.GetBalances(account.address) {
+			if b.Value.Sign() != 1 {
+				continue
+			}
+			balance = append(balance, types.Balance{
+				Coin:  uint64(b.Coin.ID),
+				Value: b.Value.String(),
+			})
+		}
+
+		// sort balances by coin symbol
+		sort.SliceStable(balance, func(i, j int) bool {
+			return bytes.Compare(types.CoinID(balance[i].Coin).Bytes(), types.CoinID(balance[j].Coin).Bytes()) == 1
+		})
+
+		acc := types.Account{
+			Address: account.address,
+			Balance: balance,
+			Nonce:   account.Nonce,
+		}
+
+		if account.IsMultisig() {
+			var weights []uint64
+			for _, weight := range account.MultisigData.Weights {
+				weights = append(weights, uint64(weight))
+			}
+			acc.MultisigData = &types.Multisig{
+				Weights:   weights,
+				Threshold: uint64(account.MultisigData.Threshold),
+				Addresses: account.MultisigData.Addresses,
+			}
+		}
+
+		if len(acc.Balance) == 0 && acc.Nonce == 0 && acc.MultisigData == nil {
+			return false
+		}
+
+		state.Accounts = append(state.Accounts, acc)
 
 		return false
 	})
