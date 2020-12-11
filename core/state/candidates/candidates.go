942b93ac66c78c1f12a7c9b046c7ccdb79b45dc0
klim0v <klim0v-sergey@yandex.ru>
2020-12-11 15:33:53 +0300

    slashed checks PunishByzantineCandidate

--- core/state/candidates/candidates.go
+++ core/state/candidates/candidates.go
@@ -295,11 +295,11 @@ func (c *Candidates) PunishByzantineCandidate(height uint64, tmAddress types.TmA
 			c.bus.Coins().SubCoinReserve(coin.ID, ret)
 
 			c.bus.App().AddTotalSlashed(ret)
-			c.bus.Checker().AddCoin(stake.Coin, big.NewInt(0).Neg(ret))
 		} else {
 			c.bus.App().AddTotalSlashed(slashed)
-			c.bus.Checker().AddCoin(stake.Coin, big.NewInt(0).Neg(slashed))
 		}
+
+		c.bus.Checker().AddCoin(stake.Coin, big.NewInt(0).Neg(slashed))
 
 		c.bus.Events().AddEvent(uint32(height), &eventsdb.SlashEvent{
 			Address:         stake.Owner,
