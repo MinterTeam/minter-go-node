5954ea2aed74802edbb7ab6fa5944ca65c2e12fa
klim0v <klim0v-sergey@yandex.ru>
2021-02-10 15:08:36 +0300

    refactor coins concurrency

--- core/state/coins/model.go
+++ core/state/coins/model.go
@@ -5,6 +5,7 @@ import (
 	"github.com/MinterTeam/minter-go-node/core/types"
 	"github.com/MinterTeam/minter-go-node/helpers"
 	"math/big"
+	"sync"
 )
 
 var minCoinReserve = helpers.BipToPip(big.NewInt(10000))
@@ -24,104 +25,178 @@ type Model struct {
 	symbolInfo *SymbolInfo
 
 	markDirty func(symbol types.CoinID)
+	lock      sync.RWMutex
 
 	isDirty   bool
 	isCreated bool
 }
 
-func (m Model) Name() string {
+func (m *Model) Name() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.CName
 }
 
-func (m Model) Symbol() types.CoinSymbol {
+func (m *Model) Symbol() types.CoinSymbol {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.CSymbol
 }
 
-func (m Model) ID() types.CoinID {
+func (m *Model) ID() types.CoinID {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.id
 }
 
-func (m Model) Crr() uint32 {
+func (m *Model) Crr() uint32 {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.CCrr
 }
 
-func (m Model) Volume() *big.Int {
+func (m *Model) Volume() *big.Int {
 	return big.NewInt(0).Set(m.info.Volume)
 }
 
-func (m Model) Reserve() *big.Int {
+func (m *Model) Reserve() *big.Int {
 	if m.IsToken() {
 		return big.NewInt(0)
 	}
+
+	m.info.RLock()
+	defer m.info.RUnlock()
+
 	return big.NewInt(0).Set(m.info.Reserve)
 }
 
-func (m Model) BaseOrHasReserve() bool {
+func (m *Model) BaseOrHasReserve() bool {
 	return m.ID().IsBaseCoin() || (m.Crr() > 0)
 }
 
-func (m Model) IsToken() bool {
+func (m *Model) IsToken() bool {
 	return !m.BaseOrHasReserve()
 }
 
-func (m Model) Version() uint16 {
+func (m *Model) Version() uint16 {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.CVersion
 }
 
-func (m Model) IsMintable() bool {
+func (m *Model) IsMintable() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.Mintable
 }
 
-func (m Model) IsBurnable() bool {
+func (m *Model) IsBurnable() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.Burnable
 }
 
 func (m *Model) SubVolume(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Volume.Sub(m.info.Volume, amount)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) AddVolume(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Volume.Add(m.info.Volume, amount)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) SubReserve(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Reserve.Sub(m.info.Reserve, amount)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) AddReserve(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Reserve.Add(m.info.Reserve, amount)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) Mint(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.CMaxSupply.Add(m.CMaxSupply, amount)
-	m.markDirty(m.id)
 	m.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) Burn(amount *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.CMaxSupply.Sub(m.CMaxSupply, amount)
-	m.markDirty(m.id)
 	m.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) SetVolume(volume *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Volume.Set(volume)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) SetReserve(reserve *big.Int) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.Lock()
 	m.info.Reserve.Set(reserve)
-	m.markDirty(m.id)
 	m.info.isDirty = true
+	m.info.Unlock()
+
+	m.markDirty(m.id)
 }
 
 func (m *Model) CheckReserveUnderflow(delta *big.Int) error {
@@ -135,27 +210,54 @@ func (m *Model) CheckReserveUnderflow(delta *big.Int) error {
 	return nil
 }
 
-func (m Model) IsInfoDirty() bool {
+func (m *Model) IsInfoDirty() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	m.info.RLock()
+	defer m.info.RUnlock()
+
 	return m.info != nil && m.info.isDirty
 }
 
-func (m Model) IsSymbolInfoDirty() bool {
-	return m.symbolInfo != nil && m.symbolInfo.isDirty
+func (m *Model) IsSymbolInfoDirty() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	has := m.symbolInfo != nil
+
+	if !has {
+		return false
+	}
+
+	m.symbolInfo.RLock()
+	defer m.symbolInfo.RUnlock()
+
+	return m.symbolInfo.isDirty
 }
 
-func (m Model) IsDirty() bool {
+func (m *Model) IsDirty() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.isDirty
 }
 
-func (m Model) IsCreated() bool {
+func (m *Model) IsCreated() bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.isCreated
 }
 
-func (m Model) MaxSupply() *big.Int {
+func (m *Model) MaxSupply() *big.Int {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.CMaxSupply
 }
 
-func (m Model) GetFullSymbol() string {
+func (m *Model) GetFullSymbol() string {
 	if m.Version() == 0 {
 		return m.Symbol().String()
 	}
@@ -168,19 +270,28 @@ type Info struct {
 	Reserve *big.Int
 
 	isDirty bool
+	sync.RWMutex
 }
 
 type SymbolInfo struct {
 	COwnerAddress *types.Address
 
 	isDirty bool
+
+	sync.RWMutex
 }
 
 func (i *SymbolInfo) setOwnerAddress(address types.Address) {
+	i.Lock()
+	defer i.Unlock()
+
 	i.COwnerAddress = &address
 	i.isDirty = true
 }
 
-func (i SymbolInfo) OwnerAddress() *types.Address {
+func (i *SymbolInfo) OwnerAddress() *types.Address {
+	i.RLock()
+	defer i.RUnlock()
+
 	return i.COwnerAddress
 }
