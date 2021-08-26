78a6b79447ad23c216fe74c95630b5cca56b9014
klim0v <klim0v-sergey@yandex.ru>
2021-08-26 20:59:12 +0300

    Revert "fix pay commission"
    This reverts commit 0c530e81d84efd4a3b82c47b0e406cf2492b9a99.

--- coreV2/transaction/edit_coin_owner.go
+++ coreV2/transaction/edit_coin_owner.go
@@ -104,7 +104,7 @@ func (data EditCoinOwnerData) Run(tx *Transaction, context state.Interface, rewa
 				detailsCom *swap.ChangeDetailsWithOrders
 				ownersCom  []*swap.OrderDetail
 			)
-			commission, commissionInBaseCoin, poolIDCom, detailsCom, ownersCom = deliverState.Swap.PairBuyWithOrders(tx.GasCoin, types.GetBaseCoinID(), commission, commissionInBaseCoin)
+			commission, commissionInBaseCoin, poolIDCom, detailsCom, ownersCom = deliverState.Swap.PairSellWithOrders(tx.GasCoin, types.GetBaseCoinID(), commission, commissionInBaseCoin)
 			tagsCom = &tagPoolChange{
 				PoolID:   poolIDCom,
 				CoinIn:   tx.CommissionCoin(),
--- coreV2/transaction/set_halt_block.go
+++ coreV2/transaction/set_halt_block.go
@@ -103,7 +103,7 @@ func (data SetHaltBlockData) Run(tx *Transaction, context state.Interface, rewar
 				detailsCom *swap.ChangeDetailsWithOrders
 				ownersCom  []*swap.OrderDetail
 			)
-			commission, commissionInBaseCoin, poolIDCom, detailsCom, ownersCom = deliverState.Swap.PairBuyWithOrders(tx.GasCoin, types.GetBaseCoinID(), commission, commissionInBaseCoin)
+			commission, commissionInBaseCoin, poolIDCom, detailsCom, ownersCom = deliverState.Swap.PairSellWithOrders(tx.GasCoin, types.GetBaseCoinID(), commission, commissionInBaseCoin)
 			tagsCom = &tagPoolChange{
 				PoolID:   poolIDCom,
 				CoinIn:   tx.CommissionCoin(),
