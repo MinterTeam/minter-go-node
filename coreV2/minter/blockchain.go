5491fe58d51433648aedba99eade95af7a17f536
klim0v <klim0v-sergey@yandex.ru>
2021-12-27 22:38:22 +0300

    refactor

--- coreV2/minter/blockchain.go
+++ coreV2/minter/blockchain.go
@@ -394,8 +394,8 @@ func (blockchain *Blockchain) EndBlock(req abciTypes.RequestEndBlock) abciTypes.
 
 	// pay rewards
 	if height%blockchain.updateStakesAndPayRewardsPeriod == 0 {
-		if blockchain.appDB.GetVersionName(height) == v3 {
-			blockchain.stateDeliver.Validators.PayRewardsV3()
+		if h := blockchain.appDB.GetVersionHeight(v3); h > 0 && height > h {
+			blockchain.stateDeliver.Validators.PayRewardsV3(height)
 		} else {
 			blockchain.stateDeliver.Validators.PayRewards()
 		}
