16b0062c67d38ad12b91c61b2aec2388ac52a9bd
klim0v <klim0v-sergey@yandex.ru>
2022-01-10 02:04:02 +0300

    fix total sun waitlist

--- coreV2/state/waitlist/model.go
+++ coreV2/state/waitlist/model.go
@@ -23,6 +23,13 @@ type Model struct {
 func (m *Model) AddToList(candidateId uint32, coin types.CoinID, value *big.Int) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
+
+	for _, item := range m.List {
+		if item.Coin == coin && item.CandidateId == candidateId {
+			item.Value.Add(item.Value, value)
+			return
+		}
+	}
 	m.List = append(m.List, &Item{
 		CandidateId: candidateId,
 		Coin:        coin,
