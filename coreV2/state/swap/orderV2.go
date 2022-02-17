cb54624ce4b4fae8ace61f69592cf70de968e941
klim0v <klim0v-sergey@yandex.ru>
2022-02-17 18:15:28 +0800

    temp burnAddress for swapV2

--- coreV2/state/swap/orderV2.go
+++ coreV2/state/swap/orderV2.go
@@ -15,7 +15,7 @@ import (
 	"github.com/cosmos/iavl"
 )
 
-var burnAddress = types.StringToAddress("MxTODO")
+var burnAddress = types.StringToAddress("Mxffffffffffffffffffffffffffffffffffffffff") // TODO
 
 func (s *SwapV2) PairSellWithOrders(coin0, coin1 types.CoinID, amount0In, minAmount1Out *big.Int) (*big.Int, *big.Int, uint32, *ChangeDetailsWithOrders, []*OrderDetail) {
 	pair := s.Pair(coin0, coin1)
