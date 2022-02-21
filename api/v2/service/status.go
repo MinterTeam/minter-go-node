db9b1aec4fa629cc5f0cf7a953022903998954e0
klim0v <klim0v-sergey@yandex.ru>
2022-02-21 23:12:58 +0800

    api status

--- api/v2/service/status.go
+++ api/v2/service/status.go
@@ -19,10 +19,6 @@ func (s *Service) Status(ctx context.Context, _ *empty.Empty) (*pb.StatusRespons
 
 	cState := s.blockchain.CurrentState()
 
-	reward, _ := cState.App().Reward()
-	if reward == nil {
-		reward = s.blockchain.RewardCounter().GetRewardForBlock(uint64(result.SyncInfo.LatestBlockHeight))
-	}
 	return &pb.StatusResponse{
 		Version:           s.version,
 		Network:           result.NodeInfo.Network,
@@ -34,7 +30,6 @@ func (s *Service) Status(ctx context.Context, _ *empty.Empty) (*pb.StatusRespons
 		KeepLastStates:    uint64(s.minterCfg.BaseConfig.KeepLastStates),
 		TotalSlashed:      cState.App().GetTotalSlashed().String(),
 		CurrentEmission:   s.blockchain.GetEmission().String(),
-		BlockReward:       reward.String(),
 		CatchingUp:        result.SyncInfo.CatchingUp,
 		PublicKey:         fmt.Sprintf("Mp%x", result.ValidatorInfo.PubKey.Bytes()[:]),
 		NodeId:            string(result.NodeInfo.ID()),
