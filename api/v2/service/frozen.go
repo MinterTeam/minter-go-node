6b639865112b2165cf096ab7a91440353a19d59c
klim0v <klim0v-sergey@yandex.ru>
2022-03-18 15:52:49 +0300

    api frozenAll

--- api/v2/service/frozen.go
+++ api/v2/service/frozen.go
@@ -35,9 +35,6 @@ func (s *Service) Frozen(ctx context.Context, req *pb.FrozenRequest) (*pb.Frozen
 	}
 	var frozen []*pb.FrozenResponse_Frozen
 
-	cState.FrozenFunds().GetFrozenFunds(s.blockchain.Height())
-
-	// todo: range iavl
 	for i := s.blockchain.Height(); i <= s.blockchain.Height()+types.GetUnbondPeriod(); i++ {
 
 		if timeoutStatus := s.checkTimeout(ctx); timeoutStatus != nil {
@@ -61,6 +58,109 @@ func (s *Service) Frozen(ctx context.Context, req *pb.FrozenRequest) (*pb.Frozen
 					continue
 				}
 			}
+			var moveToCandidateKey *wrapperspb.StringValue
+			if len(fund.MoveToCandidate) != 0 {
+				moveToCandidateKey = wrapperspb.String(cState.Candidates().PubKey(fund.MoveToCandidate[0]).String())
+			}
+			var fromCandidateKey *wrapperspb.StringValue
+			if fund.CandidateKey != nil {
+				fromCandidateKey = wrapperspb.String(fund.CandidateKey.String())
+			}
+			frozen = append(frozen, &pb.FrozenResponse_Frozen{
+				Height:       funds.Height(),
+				Address:      fund.Address.String(),
+				CandidateKey: fromCandidateKey,
+				Coin: &pb.Coin{
+					Id:     uint64(fund.Coin),
+					Symbol: coin.GetFullSymbol(),
+				},
+				Value:              fund.Value.String(),
+				MoveToCandidateKey: moveToCandidateKey,
+			})
+		}
+	}
+
+	return &pb.FrozenResponse{Frozen: frozen}, nil
+}
+
+func (s *Service) FrozenAll(ctx context.Context, req *pb.FrozenAllRequest) (*pb.FrozenResponse, error) {
+	cState, err := s.blockchain.GetStateForHeight(req.Height)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	if timeoutStatus := s.checkTimeout(ctx); timeoutStatus != nil {
+		return nil, timeoutStatus.Err()
+	}
+
+	addresses := make(map[string]struct{}, len(req.Addresses))
+	for _, address := range req.Addresses {
+		if !strings.HasPrefix(strings.Title(address), "Mx") {
+			return nil, status.Error(codes.InvalidArgument, "invalid address")
+		}
+		addresses[address] = struct{}{}
+	}
+
+	if timeoutStatus := s.checkTimeout(ctx); timeoutStatus != nil {
+		return nil, timeoutStatus.Err()
+	}
+
+	coinIDs := make(map[types.CoinID]*coins.Model, len(req.CoinIds))
+	for _, coinId := range req.CoinIds {
+		coinID := types.CoinID(coinId)
+		reqCoin := cState.Coins().GetCoin(coinID)
+		if reqCoin == nil {
+			return nil, s.createError(status.New(codes.NotFound, "Coin not found"), transaction.EncodeError(code.NewCoinNotExists("", coinID.String())))
+		}
+		coinIDs[coinID] = reqCoin
+	}
+
+	if timeoutStatus := s.checkTimeout(ctx); timeoutStatus != nil {
+		return nil, timeoutStatus.Err()
+	}
+
+	startHeight := req.StartHeight
+	if startHeight == 0 {
+		startHeight = s.blockchain.Height()
+	}
+	endHeight := req.EndHeight
+	if endHeight == 0 {
+		endHeight = startHeight + types.GetUnbondPeriod()
+	}
+
+	var frozen []*pb.FrozenResponse_Frozen
+
+	fundsAll := cState.FrozenFunds().GetFrozenFundsAll(ctx, startHeight, endHeight)
+	for _, funds := range fundsAll {
+		if funds == nil {
+			continue
+		}
+
+		for _, fund := range funds.List {
+			if len(req.Addresses) != 0 {
+				if _, ok := addresses[fund.Address.String()]; !ok {
+					continue
+				}
+			}
+
+			var coin *coins.Model
+			if len(req.CoinIds) != 0 {
+				reqCoin, ok := coinIDs[fund.Coin]
+				if !ok {
+					continue
+				}
+				coin = reqCoin
+			} else {
+				reqCoin, ok := coinIDs[fund.Coin]
+				if ok {
+					coin = reqCoin
+				} else {
+					coin = cState.Coins().GetCoin(fund.Coin)
+					coinIDs[fund.Coin] = coin
+				}
+
+			}
+
 			var moveToCandidateKey *wrapperspb.StringValue
 			if len(fund.MoveToCandidate) != 0 {
 				moveToCandidateKey = wrapperspb.String(cState.Candidates().PubKey(fund.MoveToCandidate[0]).String())
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/MinterTeam/minter-go-node
 go 1.17
 
 require (
-	github.com/MinterTeam/node-grpc-gateway v1.5.2-0.20220318103539-366bb50d46b2
+	github.com/MinterTeam/node-grpc-gateway v1.5.2-0.20220318123216-c38b87e3e274
 	github.com/btcsuite/btcd v0.22.0-beta
 	github.com/c-bata/go-prompt v0.2.5
 	github.com/cosmos/cosmos-sdk v0.44.5
