6fe44ce92c91d88737b138b3cc6f5e1e98c98bb2
klim0v <klim0v-sergey@yandex.ru>
2022-04-01 02:14:57 +0300

    list pools

--- coreV2/state/swap/best_trade.go
+++ coreV2/state/swap/best_trade.go
@@ -7,26 +7,8 @@ import (
 )
 
 func (s *Swap) GetBestTradeExactOut(ctx context.Context, inId, outId uint64, outAmount *big.Int, maxHops uint64) *Trade {
-	s.loadPools()
-
-	var pairs []EditableChecker
-
-	s.muPairs.RLock()
-	for _, pair := range s.pairs {
-		if pair == nil {
-			continue
-		}
-		reserve0, reserve1 := pair.Reserves()
-		if reserve0.Sign() < 1 || reserve1.Sign() < 1 {
-			continue
-		}
-
-		pairs = append(pairs, pair)
-	}
-	s.muPairs.RUnlock()
-
 	trade := GetBestTradeExactOut(ctx,
-		pairs,
+		s.SwapPools(),
 		types.CoinID(inId),
 		NewTokenAmount(types.CoinID(outId), outAmount),
 		maxHops,
@@ -35,48 +17,13 @@ func (s *Swap) GetBestTradeExactOut(ctx context.Context, inId, outId uint64, out
 	return trade
 }
 func (s *SwapV2) GetBestTradeExactOut(ctx context.Context, inId, outId uint64, outAmount *big.Int, maxHops uint64) *Trade {
-	s.loadPools()
-
-	var pairs []EditableChecker
-
-	s.muPairs.RLock()
-	for _, pair := range s.pairs {
-		if pair == nil {
-			continue
-		}
-		reserve0, reserve1 := pair.Reserves()
-		if reserve0.Sign() < 1 || reserve1.Sign() < 1 {
-			continue
-		}
-
-		pairs = append(pairs, pair)
-	}
-	s.muPairs.RUnlock()
-
-	trade := GetBestTradeExactOut(ctx, pairs, types.CoinID(inId), NewTokenAmount(types.CoinID(outId), outAmount), maxHops)
+	trade := GetBestTradeExactOut(ctx, s.SwapPools(), types.CoinID(inId), NewTokenAmount(types.CoinID(outId), outAmount), maxHops)
 
 	return trade
 }
 func (s *Swap) GetBestTradeExactIn(ctx context.Context, outId, inId uint64, inAmount *big.Int, maxHops uint64) *Trade {
-	s.loadPools()
-
-	var pairs []EditableChecker
-
-	s.muPairs.RLock()
-	for _, pair := range s.pairs {
-		if pair == nil {
-			continue
-		}
-		reserve0, reserve1 := pair.Reserves()
-		if reserve0.Sign() < 1 || reserve1.Sign() < 1 {
-			continue
-		}
-		pairs = append(pairs, pair)
-	}
-	s.muPairs.RUnlock()
-
 	trades := GetBestTradeExactIn(ctx,
-		pairs,
+		s.SwapPools(),
 		types.CoinID(outId),
 		NewTokenAmount(types.CoinID(inId), inAmount),
 		maxHops,
@@ -85,24 +32,8 @@ func (s *Swap) GetBestTradeExactIn(ctx context.Context, outId, inId uint64, inAm
 	return trades
 }
 func (s *SwapV2) GetBestTradeExactIn(ctx context.Context, outId, inId uint64, inAmount *big.Int, maxHops uint64) *Trade {
-	s.loadPools()
 
-	var pairs []EditableChecker
-
-	s.muPairs.RLock()
-	for _, pair := range s.pairs {
-		if pair == nil {
-			continue
-		}
-		reserve0, reserve1 := pair.Reserves()
-		if reserve0.Sign() < 1 || reserve1.Sign() < 1 {
-			continue
-		}
-		pairs = append(pairs, pair)
-	}
-	s.muPairs.RUnlock()
-
-	trades := GetBestTradeExactIn(ctx, pairs, types.CoinID(outId), NewTokenAmount(types.CoinID(inId), inAmount), maxHops)
+	trades := GetBestTradeExactIn(ctx, s.SwapPools(), types.CoinID(outId), NewTokenAmount(types.CoinID(inId), inAmount), maxHops)
 
 	return trades
 }
