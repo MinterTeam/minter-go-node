1f2f1562f7e49997d69ce7ae744898e492f8af93
klim0v <klim0v-sergey@yandex.ru>
2022-03-31 15:07:32 +0300

    fix error message

--- api/v2/service/estimate_coin_sell.go
+++ api/v2/service/estimate_coin_sell.go
@@ -258,6 +258,9 @@ func (s *Service) calcSellFromPool(ctx context.Context, value *big.Int, cState *
 		coinBuy := coinTo
 		if buyCoinID != coinBuy.ID() {
 			coinBuy = cState.Coins().GetCoin(buyCoinID)
+			if coinBuy == nil {
+				return nil, s.createError(status.New(codes.NotFound, "Coin to sell not exists"), transaction.EncodeError(code.NewCoinNotExists("", buyCoinID.String())))
+			}
 		}
 		if !swapChecker.Exists() {
 			return nil, s.createError(status.New(codes.NotFound, fmt.Sprintf("swap pool between coins %s and %s not exists", coinSell.GetFullSymbol(), coinBuy.GetFullSymbol())), transaction.EncodeError(code.NewPairNotExists(coinSell.ID().String(), coinBuy.ID().String())))
