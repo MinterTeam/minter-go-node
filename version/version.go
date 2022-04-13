a2675f2385cda1962151947e60d26260d1de7f4a
klim0v <klim0v-sergey@yandex.ru>
2022-04-13 18:20:35 +0300

    version

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ const (
 
 var (
 	// Version must be a string because scripts like dist.sh read this file.
-	Version = "3.0.3"
+	Version = "3.1.0"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
