c294035a86aa5bdf57faf830047ecc3858d75010
klim0v <klim0v-sergey@yandex.ru>
2022-04-18 15:55:22 +0300

    update state-sync default params

--- config/config.go
+++ config/config.go
@@ -78,8 +78,8 @@ func DefaultConfig() *Config {
 	//cfg.StateSync.RPCServers = []string{"sync-test.minter.network:26657", "sync104.minter.su:26657", "state-test.minter.network:26657"}
 	cfg.StateSync.RPCServers = []string{"state-test.minter.network:26657", "sync-test.minter.network:26657", "sync101.minter.su:26657"}
 
-	cfg.StateSync.TrustHeight = 10197600
-	cfg.StateSync.TrustHash = "BE035F6C00320FBB76D8B3C427891A03D46B3443A9E4309D777ABC4EAB4FDFCC"
+	cfg.StateSync.TrustHeight = 10309900
+	cfg.StateSync.TrustHash = "95AB9C70AE7B98F955CB05039A3B2A442F3DA4F95FF82B027A8144AE0E15C6BF"
 	cfg.StateSync.TrustPeriod = time.Hour * 8760
 
 	return cfg
