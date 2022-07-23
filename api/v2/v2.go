85e3d669722d393ba5f704a71c4cc717e15cfd62
klim0v <klim0v-sergey@yandex.ru>
2022-07-23 14:48:43 +0300

    fix rw mutex

--- api/v2/v2.go
+++ api/v2/v2.go
@@ -4,6 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/MinterTeam/minter-go-node/api/v2/service"
 	gw "github.com/MinterTeam/node-grpc-gateway/api_pb"
 	"github.com/MinterTeam/node-grpc-gateway/docs"
@@ -25,13 +33,6 @@ import (
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
 	_struct "google.golang.org/protobuf/types/known/structpb"
-	"io"
-	"mime"
-	"net"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
 )
 
 // Run initialises gRPC and API v2 interfaces
